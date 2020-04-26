d4d1a87fa11dcc4b5ff913f9f0827584ab23323a
Peter Stradinger <emlprime@gmail.com>
2020-04-26 08:16:40 -0700

    Added favicon and title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import GlobalStyle from "./GlobalStyle";
+import Header from "./Header";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <header>
-        <h1>Tau Tao</h1>
-        <nav>breadcrumb</nav>
-        <main>Main</main>
-      </header>
+      <Header />
+
+      <main>Main</main>
     </div>
   );
 }
