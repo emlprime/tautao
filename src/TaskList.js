a00136109898313198e5946ab6b23d7bf1eaf03e
Peter Stradinger <emlprime@gmail.com>
2020-05-02 13:39:44 -0700

    calculating the total points

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,10 +1,23 @@
 import React, { useCallback } from "react";
+import * as R from "ramda";
 import RankList from "./RankList";
 import styled from "styled-components";
 import Progress from "./Progress";
 import Points from "./Points";
+import { useStore } from "./StoreContext";
+
+const getPoints = R.curry((state, model, id) => R.path(["byId", model, id, "points"], state));
 
 const TaskList = ({ rootIds }) => {
+  const { state } = useStore();
+  const getPointsForState = getPoints(state);
+  const getPointsByItem = R.converge(getPointsForState, [R.prop("model"), R.prop("id")]);
+  const totalPoints = R.pipe(
+    R.map(getPointsByItem),
+    R.sum
+  )(rootIds);
+  console.log("points:", totalPoints);
+
   const name = "Milestones";
   const handleNewOrder = useCallback(order => {
     console.log("order:", order);
@@ -14,7 +27,7 @@ const TaskList = ({ rootIds }) => {
       <header>
         <h2>{name}</h2>
         <Totals>
-          <Points points={23} />
+          <Points points={totalPoints} />
           <Progress />
         </Totals>
       </header>
